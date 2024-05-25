d551c52a7fdf5ccbd595295d5be42e7d17a52b0a
geopokrovskiy <99304283+geopokrovskiy@users.noreply.github.com>
2024-05-25 16:28:52 +0200

    Initial commit

---
+++ src/main/java/com/geopokrovskiy/keycloakAuthenticator/KeycloakAuthenticatorApplication.java
@@ -0,0 +1,13 @@
+package com.geopokrovskiy.keycloakAuthenticator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakAuthenticatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakAuthenticatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geopokrovskiy/keycloakAuthenticator/KeycloakAuthenticatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geopokrovskiy.keycloakAuthenticator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakAuthenticatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
